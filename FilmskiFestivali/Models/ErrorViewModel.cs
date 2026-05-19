b9cdbb53c543ec5d0122afbcf559d5d6ee2ac8dd
annaopi <opancinaana0902@gmail.com>
2026-05-19 20:09:41 +0200

    FilmskiFestivali

---
+++ FilmskiFestivali/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FilmskiFestivali.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
